7c4e59958f5ecf6b9d12da2454e4ae885c6fd1c7
mu <cs.21.065@student.uotechnology.edu.iq>
2023-06-30 16:32:46 +0300

    a

--- SuperHeroApi/Program.cs
+++ SuperHeroApi/Program.cs
@@ -2,16 +2,19 @@ using Microsoft.EntityFrameworkCore;
 using SuperHeroApi.Data;
 using SuperHeroApi.Interfaces;
 using SuperHeroApi.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using HotChocolate.AspNetCore;
+using HotChocolate.AspNetCore.Playground;
 
 var builder = WebApplication.CreateBuilder(args);
-ConfigurationManager configuration = builder.Configuration;
+var configuration = builder.Configuration;
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 // Register custom services for the superheroes
 builder.Services.AddScoped<ISuperheroRepository, SuperheroRepository>();
@@ -21,23 +24,23 @@ builder.Services.AddGraphQLServer().AddQueryType<Query>().AddProjections().AddFi
 
 // Add Application Db Context options
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-options.UseSqlServer(configuration.GetConnectionString("SqlServer")));
+    options.UseSqlServer(configuration.GetConnectionString("SqlServer")));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
-app.UseHttpsRedirection();
-
+app.UseRouting();
 app.UseAuthorization();
 
-app.MapControllers();
-
-app.MapGraphQL("/graphql");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapGraphQL();
+});
 
 app.Run();
